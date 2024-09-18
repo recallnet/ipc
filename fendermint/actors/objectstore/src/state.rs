a102de56569055432d229d36ff7c187dd0d84b56
Dan Buchholz <dbuchholz30@gmail.com>
2024-09-18 12:17:30 -0400

    chore: rebase fixes

--- fendermint/actors/objectstore/src/state.rs
+++ fendermint/actors/objectstore/src/state.rs
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0, MIT
 
 use cid::Cid;
-use fendermint_actor_blobs_shared::Hash;
+use fendermint_actor_blobs_shared::state::Hash;
 use fendermint_actor_machine::{Kind, MachineState, WriteAccess};
 use fil_actors_runtime::ActorError;
 use fvm_ipld_blockstore::Blockstore;
