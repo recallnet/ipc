bbdf7abb05410fd25d21b92e58f8fe8828d9546a
Joe Wagner <njwjs722@gmail.com>
2025-01-30 12:35:45 -0700

    rename contribs with Recall

--- recall/ipld/src/amt/core.rs
+++ recall/ipld/src/amt/core.rs
@@ -1,4 +1,4 @@
-// Copyright 2024 Hoku Contributors
+// Copyright 2025 Recall Contributors
 // Copyright 2022-2024 Protocol Labs
 // Copyright 2019-2022 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
