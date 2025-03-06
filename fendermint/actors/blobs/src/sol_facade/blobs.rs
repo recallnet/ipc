ba73430f584f55fd99d960c9bac7814f739058f8
Sergey Ukustov <sergey@ukstv.me>
2025-03-06 23:00:35 +0300

    BlobDeleted

--- fendermint/actors/blobs/src/sol_facade/blobs.rs
+++ fendermint/actors/blobs/src/sol_facade/blobs.rs
@@ -2,7 +2,7 @@ use anyhow::Error;
 use recall_sol_facade::blobs as sol;
 use fvm_shared::address::Address;
 use fvm_shared::clock::ChainEpoch;
-use fendermint_actor_blobs_shared::state::Hash;
+use fendermint_actor_blobs_shared::state::{Hash, PublicKey};
 use recall_actor_sdk::TryIntoEVMEvent;
 use recall_sol_facade::primitives::U256;
 use recall_sol_facade::types::H160;
@@ -26,6 +26,59 @@ impl TryIntoEVMEvent for BlobAdded<'_> {
             size: U256::from(self.size),
             expiry: U256::from(self.expiry),
             bytesUsed: U256::from(self.bytes_used),
+        }))
+    }
+}
+
+pub struct BlobPending<'a> {
+    pub subscriber: Address,
+    pub hash: &'a Hash,
+    pub source: &'a PublicKey
+}
+impl TryIntoEVMEvent for BlobPending<'_> {
+    type Target = sol::Event;
+    fn try_into_evm_event(self) -> Result<sol::Event, Error> {
+        let subscriber: H160 = self.subscriber.try_into()?;
+        Ok(sol::Event::BlobPending(sol::BlobPending {
+            subscriber: subscriber.into(),
+            hash: self.hash.0.into(),
+            sourceId: self.source.0.into(),
+        }))
+    }
+}
+
+pub struct BlobFinalized<'a> {
+    pub subscriber: Address,
+    pub hash: &'a Hash,
+    pub resolved: bool,
+}
+impl TryIntoEVMEvent for BlobFinalized<'_> {
+    type Target = sol::Event;
+    fn try_into_evm_event(self) -> Result<sol::Event, Error> {
+        let subscriber: H160 = self.subscriber.try_into()?;
+        Ok(sol::Event::BlobFinalized(sol::BlobFinalized {
+            subscriber: subscriber.into(),
+            hash: self.hash.0.into(),
+            resolved: self.resolved,
+        }))
+    }
+}
+
+pub struct BlobDeleted<'a> {
+    pub subscriber: Address,
+    pub hash: &'a Hash,
+    pub size: u64,
+    pub bytes_released: u64,
+}
+impl TryIntoEVMEvent for BlobDeleted<'_> {
+    type Target = sol::Event;
+    fn try_into_evm_event(self) -> Result<sol::Event, Error> {
+        let subscriber: H160 = self.subscriber.try_into()?;
+        Ok(sol::Event::BlobDeleted(sol::BlobDeleted {
+            subscriber: subscriber.into(),
+            hash: self.hash.0.into(),
+            size: U256::from(self.size),
+            bytesReleased: U256::from(self.bytes_released),
         }))
     }
 }
