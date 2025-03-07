8a0f291f6577561e44fb47f62f0b767163814645
Sergey Ukustov <sergey@ukstv.me>
2025-03-07 14:41:11 +0300

    declare abi call

--- fendermint/actors/blobs/src/sol_facade/mod.rs
+++ fendermint/actors/blobs/src/sol_facade/mod.rs
@@ -2,9 +2,9 @@
 // Copyright 2022-2024 Protocol Labs
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-use recall_actor_sdk::declare_abi_encode_returns;
+use recall_actor_sdk::declare_abi_call;
 
-declare_abi_encode_returns!();
+declare_abi_call!();
 
 pub mod blobs;
 pub mod credit;
