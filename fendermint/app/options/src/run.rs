5dcff3c4ac6ba0b6d2b82a3c586f031ed008c437
dignifiedquire <me@dignifiedquire.com>
2025-03-10 17:53:03 +0100

    initial conversion to iroh@0.33

--- fendermint/app/options/src/run.rs
+++ fendermint/app/options/src/run.rs
@@ -1,10 +1,12 @@
 // Copyright 2022-2024 Protocol Labs
 // SPDX-License-Identifier: Apache-2.0, MIT
 
+use std::path::PathBuf;
+
 use clap::Args;
 
 #[derive(Args, Debug)]
 pub struct RunArgs {
-    #[arg(long, short, default_value = "127.0.0.1:4919", env = "IROH_RPC_ADDR")]
-    pub iroh_addr: String,
+    #[arg(long, short, env = "IROH_OBJECTS_PATH")]
+    pub iroh_path: PathBuf,
 }
