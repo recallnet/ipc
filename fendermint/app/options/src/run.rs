83d522312bccf46f53597fa4b4a50ee8fb0613a2
dignifiedquire <me@dignifiedquire.com>
2025-03-19 22:12:04 +0100

    cleanups

--- fendermint/app/options/src/run.rs
+++ fendermint/app/options/src/run.rs
@@ -4,14 +4,10 @@
 use std::path::PathBuf;
 
 use clap::Args;
-use iroh_base::NodeId;
 
 #[derive(Args, Debug)]
 pub struct RunArgs {
     /// Storage path for the iroh node
     #[arg(long, env = "IROH_PATH")]
     pub iroh_path: PathBuf,
-    /// Storage path for the syscall iroh node
-    #[arg(long, env = "IROH_OBJECTS_NODE_ID")]
-    pub iroh_objects_id: NodeId,
 }
